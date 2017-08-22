3fbb7de373a0c9d6c8048be617bf97a1e4a23397
Matthew Duphily <matthew.duphily@live.com>
2017-08-22 00:44:59 -0400

    Added bobbing paper with functionality of picking it up and displaying its contents

---
+++ Assets/Scripts/Paper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper : MonoBehaviour {
+
+    public string command;
+    public string description;
+    public bool collected;
+
+	// Use this for initialization
+	void Start () {
+        collected = false;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        collected = true;
+        gameObject.SetActive(false);
+    }
+}
